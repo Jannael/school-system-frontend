761b1529ccb1daabe908660121da6c722e5633f1
Jannael Orlando <orlandojannael@gmail.com>
2025-11-20 16:04:31 -0600

    score model

--- model/score/model.js
+++ model/score/model.js
@@ -8,6 +8,28 @@ const model = {
       credentials: 'include'
     })
     return res.json()
+  },
+  newSemester: async function (subject) {
+    const res = await fetch(`${api}/newSemester/`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ subject })
+    })
+    return res.json()
+  },
+  update: async function (account, semester, subject, score, partial) {
+    const res = await fetch(`${api}/newSemester/`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ subject, account, semester, score, partial })
+    })
+    return res.json()
   }
 }
 
